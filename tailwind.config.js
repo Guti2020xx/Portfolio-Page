c4e9236ff839f8b8010508c799b378eb6d819a23
Guti2020xx <54343329+Guti2020xx@users.noreply.github.com>
2025-08-28 03:51:30 -0500

    Deleted Config stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,16 +29,6 @@ export default {
         display: ["Playfair Display", "serif"],
         sans: ["Inter", "sans-serif"],
       },
-      keyframes: {
-        gradientShift: {
-          "0%": { "background-position": "0% 50%" },
-          "50%": { "background-position": "100% 50%" },
-          "100%": { "background-position": "0% 50%" },
-        },
-      },
-      animation: {
-        gradientShift: "gradientShift 10s ease infinite",
-      },
     },
   },
   plugins: [...defaultConfig.plugins, tailwindcssAnimate],
